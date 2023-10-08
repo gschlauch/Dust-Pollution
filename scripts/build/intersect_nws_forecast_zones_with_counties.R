a64c6036c6137caea317e0285215554ece7c44fe
Gary Schlauch <garyschlauch@Garys-MacBook-Pro.local>
2023-10-07 20:36:12 -0700

    made changes to handle errors in invalid geometries. the forecast zone file still needs work

--- scripts/build/intersect_nws_forecast_zones_with_counties.R
+++ scripts/build/intersect_nws_forecast_zones_with_counties.R
@@ -25,16 +25,16 @@ county_shp <- st_read(paste0(path_data_int, "/census/2020/county/US_county_2020.
 
 # Initialize a zone-to-county 1:m crosswalk. This will contain all zone files
 # and is updated in the for loop below
-xwalk <- data.frame()
+xwalk_df <- data.frame()
 
 # Get forecast zone files
 zone_files <- list.files(
-  paste0(path_data_raw, "/nws/forecast_zones"), 
+  paste0(path_data_raw, "/dust_storms/forecast_zones"), 
   full.names = TRUE
 )
 
 # Loop through forecast zones
-for (i in 1:1) { #(i in 1:length(zone_files)) {
+for (i in 5:length(zone_files)) {# (i in 1:length(zone_files)) {
   
   print(paste0("File ", i, "/", length(zone_files)))
   
@@ -42,11 +42,25 @@ for (i in 1:1) { #(i in 1:length(zone_files)) {
   temp_files <- list.files(path_temp, full.names = TRUE)
   file.remove(temp_files)
   
-  # Unzip, load, and clean the zones file
-  print("Cleaning zone shapefile...")
+  # Unzip and load
+  print("Prepping zone shapefile...")
   unzip(zone_files[i], exdir = path_temp)
   zone_shp_filename <- list.files(path_temp, pattern = ".shp")
-  zone_shp <- st_read(paste0(path_temp, "/", zone_shp_filename)) %>%
+  zone_shp <- st_read(paste0(path_temp, "/", zone_shp_filename)) 
+  
+  # Some of the shapefiles are missing a CRS. Since all of the other shapefiles
+  # had NAD83 as the CRS, I guess that the missing CRS is NAD83
+  crs <- st_crs(zone_shp)[[1]]
+  if (!is.na(crs)) {
+    if (crs != "NAD83") {
+      stop("The CRS is not NAD83")
+    }
+  } else {
+    zone_shp <- zone_shp %>% st_set_crs("NAD83")
+  }
+  
+  # Basic tidying
+  zone_shp <- zone_shp %>%
     dplyr::select(STATE, ZONE, NAME) %>%
     dplyr::rename(zone_state = STATE, zone_fips = ZONE, zone_name = NAME) %>%
     mutate_at(c("zone_state", "zone_fips", "zone_name"), ~tidy_string(.)) %>%
@@ -57,8 +71,29 @@ for (i in 1:1) { #(i in 1:length(zone_files)) {
     st_transform("WGS84") %>%
     st_make_valid()
   
+  # Union the geometries for zones that are listed twice. This is annoying since
+  # st_union isn't working for some of the geometries. However, the ones it isn't
+  # working for are already unique geometries (ie, only have 1 row in zone_shp).
+  # So I split the process into two parts here.
+  print("Unioning geometries within state-fips-name")
+  n_distinct_zones <- nrow(distinct(as.data.frame(zone_shp), zone_state, zone_fips, zone_name))
+  zone_shp <- zone_shp %>% 
+    group_by(zone_state, zone_fips, zone_name, zone_filename) %>%
+    dplyr::mutate(obs = n()) %>%
+    ungroup()
+  zone_shp_1 <- zone_shp %>% filter(obs == 1)
+  zone_shp_2 <- zone_shp %>% 
+    filter(obs > 1) %>%
+    group_by(zone_state, zone_fips, zone_name, zone_filename) %>%
+    dplyr::summarize(geometry = st_union(geometry)) %>%
+    ungroup() %>%
+    st_make_valid()
+  zone_shp <- bind_rows(zone_shp_1, zone_shp_2)
+  if (nrow(zone_shp) != n_distinct_zones) {
+    stop("I don't have the correct number of zones after doing the union")
+  }
+  
   # Loop through states
-  start_time <- Sys.time()
   for (state in state_abbreviations) {
     
     print(paste0("Processing for : ", state))
@@ -66,11 +101,23 @@ for (i in 1:1) { #(i in 1:length(zone_files)) {
     # Filter the zone shapefile to just that state
     zone_shp_state <- zone_shp %>% filter(zone_state == state)
     
+    # Get the number of unique zones to compare at the end of this loop
+    n_unique_zone_shp_state <- as.data.frame(zone_shp_state) %>% 
+      dplyr::select(zone_state, zone_fips, zone_name) %>%
+      n_distinct()
+    
     # Filter the county shapefile to just that state and the surrounding states
     county_shp_state <- county_shp %>% 
       filter(stabv %in% get_neighboring_states(state))
     
-    # Get a list of county indices that intersect at all with each zone
+    # Get a list of county indices that intersect at all with each zone. For some
+    # annoying reason, I can't get one of the geometries to be valid for 
+    # "MN" in the file "z_01ap14.shp". to handle this case, I set sf_use_s2 to false
+    if (state == "MN" & zone_shp_filename == "z_01ap14.shp") {
+      zone_shp_state_bad <- zone_shp_state[96, ]
+      zone_shp_state <- zone_shp_state[-96, ]
+    }
+    
     intersections_list <- st_intersects(zone_shp_state, county_shp_state)
     
     # For each zone, get the polygon that is the intersection of zone-counties
@@ -79,16 +126,43 @@ for (i in 1:1) { #(i in 1:length(zone_files)) {
     )
     if (nrow(zone_shp_state) > 1) {
       for (k in 2:nrow(zone_shp_state)) {
-        intersections_shp_k <- st_intersection(
-          zone_shp_state[k, ], county_shp_state[intersections_list[[k]], ]
-        )
+        
+        # These ones gives an error for whatever reason but it works when I use s2.
+        # I also get a warning if I do the intersection in latitude/longitude, so
+        # I transform to planar as well
+        bad_cases <- 
+          (k == 76 & state == "NY" & zone_shp_filename == "z_01de10.shp") |
+          (k == 94 & state == "NC" & zone_shp_filename == "z_01de10.shp")
+        if (bad_cases) {
+          sf_use_s2(FALSE)
+          intersections_shp_k <- st_intersection(
+            st_transform(zone_shp_state[k, ], 3857), 
+            st_transform(county_shp_state[intersections_list[[k]], ], 3857)
+          ) %>% st_transform(4326)
+          sf_use_s2(TRUE)
+        } else { # Do it normally for the rest
+          intersections_shp_k <- st_intersection(
+            zone_shp_state[k, ], county_shp_state[intersections_list[[k]], ]
+          )
+        }
         intersections_shp <- bind_rows(intersections_shp, intersections_shp_k)
       }
+    }
+    
+    # Add in the "bad" geometries
+    # This one works if I project to a different CRS
+    if (state == "MN" & zone_shp_filename == "z_01ap14.shp") {
+      zone_shp_state_bad <- zone_shp_state_bad %>% st_transform(3857) %>% st_make_valid()
+      county_shp_state_bad <- county_shp_state %>% st_transform(3857)
+      intersections_shp_bad <- st_intersection(zone_shp_state_bad, county_shp_state_bad) %>%
+        st_transform(4326)
+      intersections_shp <- bind_rows(intersections_shp, intersections_shp_bad)
     }
     
     # Compute the percent of area overlap between the zones and counties. That is
     # (area_overlap / area_county) * 100
     intersections_shp <- intersections_shp %>%
+      st_make_valid() %>%
       mutate(
         intersection_area = st_area(.),
         pct_county_overlap = as.numeric((intersection_area / cntyarea) * 100)
@@ -99,46 +173,68 @@ for (i in 1:1) { #(i in 1:length(zone_files)) {
       dplyr::select(zone_state, zone_fips, zone_name, zone_filename, stabv, 
                     stfp, cntyfp, pct_county_overlap)
     
-    # Add to the final crosswalk
-    xwalk <- bind_rows(xwalk, intersections_df)
+    # Check that pct_county_overlap is between 0 and 1
+    values <- intersections_df$pct_county_overlap
+    if (any(is.na(values)) | any(values < 0) | any(values > 100.1)) {
+      stop("pct_county_overlap is not between 0 and 100 or is missing")
+    }
+    intersections_df <- intersections_df %>%
+      mutate(pct_county_overlap = ifelse(pct_county_overlap > 100, 100, pct_county_overlap))
+    
+    # Check that the data are uniquely identified by zone-county
+    check_df_unique_by(intersections_df, zone_state, zone_fips, stfp, cntyfp)
+    
+    # Check that I have the correct number of unique zones comparing the shapefile
+    # and final dataframe
+    n_unique_intersections_df <- intersections_df %>%
+      dplyr::select(zone_state, zone_fips) %>%
+      n_distinct()
+    if (n_unique_zone_shp_state != n_unique_intersections_df) {
+      stop("The number of unique zones is incorrect in the final dataframe compared to the shapefile")
+    }
+    
+    # Append to the final crosswalk
+    xwalk_df <- bind_rows(xwalk_df, intersections_df)
     
   }
 }
 
-row.names(xwalk) <- NULL
-write_csv(paste0(path_int, "/walks/xwalk_NWS_forecast_zones_to_counties.csv"))
+row.names(xwalk_df) <- NULL
+write_csv(xwalk_df, paste0(path_data_int, "/Crosswalks/xwalk_NWS_forecast_zones_to_counties.csv"))
 
-# Quick maps to check the output
-xwalk_temp <- xwalk %>% 
-  arrange(desc(pct_county_overlap)) %>%
-  filter(row_number() %in% c(1:10, 90:110, 500:510, 200:210))
-distinct_zones <- xwalk_temp %>% distinct(zone_state, zone_fips)
-for (i in 22:22) { #(i in 1:nrow(distinct_zones)) {
-  
-  zone_state_i <- distinct_zones$zone_state[i]
-  zone_fips_i <- distinct_zones$zone_fips[i]
-  zone_shp_temp <- zone_shp %>%
-    filter(zone_state == zone_state_i, zone_fips == zone_fips_i)
-  county_shp_temp <- xwalk %>%
-    dplyr::filter(zone_state == zone_state_i, zone_fips == zone_fips_i) %>%
-    dplyr::select(stfp, cntyfp, pct_county_overlap) %>%
-    left_join(county_shp, by = c("stfp", "cntyfp")) %>%
-    mutate(overlap_threshold = ifelse(pct_county_overlap >= 5, 1, 0))
 
-  map <- ggplot() +
-    geom_sf(
-      data = county_shp_temp,
-      aes(fill = overlap_threshold, geometry = geometry),
-      lwd = 0.5,
-      color = "green"
-    ) +
-    geom_sf(
-      data = zone_shp_temp,
-      lwd = 0.5,
-      alpha = 0,
-      color = "red"
-    )
-  ggsave(paste0("/users/garyschlauch/downloads/map_", i, ".png"))
-}
 
+# # Quick maps to check the output
+# zone_filename_i <- "z_01ap08.shp"
+# distinct_zones <- xwalk_df %>% 
+#   filter(zone_filename == zone_filename_i) %>%
+#   distinct(zone_state, zone_fips)
+# rand <- sample(1:nrow(distinct_zones), 20, replace = F)
+# for (i in rand) {
+# 
+#   zone_state_i <- distinct_zones$zone_state[i]
+#   zone_fips_i <- distinct_zones$zone_fips[i]
+#   zone_shp_temp <- zone_shp %>%
+#     filter(zone_state == zone_state_i, zone_fips == zone_fips_i)
+#   county_shp_temp <- xwalk_df %>%
+#     dplyr::filter(zone_state == zone_state_i, zone_fips == zone_fips_i) %>%
+#     dplyr::select(stfp, cntyfp, pct_county_overlap) %>%
+#     left_join(county_shp, by = c("stfp", "cntyfp")) %>%
+#     mutate(overlap_threshold = ifelse(pct_county_overlap >= 5, 1, 0))
+# 
+#   map <- ggplot() +
+#     geom_sf(
+#       data = county_shp_temp,
+#       aes(fill = overlap_threshold, geometry = geometry),
+#       lwd = 0.5,
+#       color = "green"
+#     ) +
+#     geom_sf(
+#       data = zone_shp_temp,
+#       lwd = 0.5,
+#       alpha = 0,
+#       color = "red"
+#     )
+#   ggsave(paste0("/users/garyschlauch/downloads/map_", i, ".png"))
+# }
 
---
+++ scripts/build/process_nws_warnings.R
@@ -0,0 +1,126 @@
+#-------------------------------------------------------------------------------
+# Process NWS dust storm warnings warnings data
+# Written by: Gary Schlauch
+#-------------------------------------------------------------------------------
+
+setwd("/Users/garyschlauch/Documents/github/Dust-Pollution")
+source("scripts/setup/00_load_settings.R")
+source("scripts/setup/00_load_packages.R")
+source("scripts/setup/00_load_functions.R")
+
+# Intersect warnings with counties ---------------------------------------------
+
+# Load the county shapefile
+county_shp <- st_read(paste0(path_data_int, "/census/2020/county/US_county_2020.shp")) %>%
+  dplyr::select(-cntyname)
+
+# Initialize a warning-to-county 1:m crosswalk. This will contain all warning files
+# and is updated in the for loop below
+xwalk_df <- data.frame()
+
+# Loop through storm warning files
+years <- as.character(2005:2022)
+for (year in years) {
+  
+  print(paste0("Year: ", year))
+  
+  print("Loading and prepping warnings shapefile")
+  filepath <- paste0(
+    path_data_raw, 
+    "/dust_storms/storm_warnings/", 
+    year, 
+    "_all/wwa_", 
+    year, 
+    "01010000_", 
+    year, 
+    "12312359.shp"
+  )
+  warning_shp <- st_read(filepath) %>%
+    filter(PHENOM %in% c("DS", "DU")) %>%
+    dplyr::select(WFO, ISSUED, EXPIRED, geometry) %>%
+    st_transform("WGS84") %>%
+    st_make_valid() %>%
+    distinct()
+  names(warning_shp) <- str_to_lower(names(warning_shp))
+  
+  # Union warnings that have multiple rows in the shapefile. I split this into 
+  # two parts for speed and to make sure nothing goes weird with the warnings that
+  # only have 1 row when doing the union
+  n_distinct_warnings <- nrow(distinct(as.data.frame(warning_shp), wfo, issued, expired))
+  warning_shp <- warning_shp %>% 
+    group_by(wfo, issued, expired) %>%
+    dplyr::mutate(obs = n()) %>%
+    ungroup()
+  warning_shp_1 <- warning_shp %>% filter(obs == 1)
+  warning_shp_2 <- warning_shp %>% 
+    filter(obs > 1) %>%
+    group_by(wfo, issued, expired) %>%
+    dplyr::summarize(geometry = st_union(geometry)) %>%
+    ungroup() %>%
+    st_make_valid()
+  warning_shp <- bind_rows(warning_shp_1, warning_shp_2)
+  if (nrow(warning_shp) != n_distinct_warnings) {
+    stop("I don't have the correct number of warnings after doing the union")
+  }
+  
+  # Get a list of county indices that intersect at all with each warning polygon
+  print("Intersecting warnings with counties (binary)")
+  intersections_list <- st_intersects(warning_shp, county_shp)
+  
+  # For each warning, get the polygon that is the intersection of warning-counties
+  print("Obtaining actual intersection polygons")
+  intersections_shp <- st_intersection(
+    warning_shp[1, ], county_shp[intersections_list[[1]], ]
+  )
+  if (nrow(warning_shp) > 1) {
+    for (k in 2:nrow(warning_shp)) {
+      intersections_shp_k <- st_intersection(
+        warning_shp[k, ], county_shp[intersections_list[[k]], ]
+      )
+      intersections_shp <- bind_rows(intersections_shp, intersections_shp_k)
+    }
+  }
+
+  # Compute the percent of area overlap between the warnings and counties. That is
+  # (area_overlap / area_county) * 100
+  print("Computing areas")
+  intersections_shp <- intersections_shp %>%
+    st_make_valid() %>%
+    mutate(
+      intersection_area = st_area(.),
+      pct_county_overlap = as.numeric((intersection_area / cntyarea) * 100)
+    )
+  
+  print("Wrapping up")
+  # Convert to a dataframe
+  intersections_df <- as.data.frame(intersections_shp) %>%
+    dplyr::select(-c(cntyarea, intersection_area, geometry))
+  
+  # Check that pct_county_overlap is between 0 and 1
+  values <- intersections_df$pct_county_overlap
+  if (any(is.na(values)) | any(values < 0) | any(values > 100.1)) {
+    stop("pct_county_overlap is not between 0 and 100 or is missing")
+  }
+  intersections_df <- intersections_df %>%
+    mutate(pct_county_overlap = ifelse(pct_county_overlap > 100, 100, pct_county_overlap))
+  
+  # Check that the data are uniquely identified by warning-county
+  check_df_unique_by(intersections_df, wfo, issued, expired, stfp, cntyfp)
+  
+  # Append to the final crosswalk
+  xwalk_df <- bind_rows(xwalk_df, intersections_df)
+  
+}
+
+row.names(xwalk_df) <- NULL
+
+# Create R date-time variables (all times are already in UTC)
+xwalk_df <- xwalk_df %>%
+  mutate(
+    warning_start_date = ymd(str_sub(issued, 1, 8)),
+    warning_start_datetime = ymd_hm(issued),
+    warning_end_date = ymd(str_sub(expired, 1, 8)),
+    warning_end_datetime = ymd_hm(expired)
+  )
+
+write_csv(xwalk_df, paste0(path_data_int, "/Dust_storms/NWS_dust_storm_warnings_cleaned.csv"))
